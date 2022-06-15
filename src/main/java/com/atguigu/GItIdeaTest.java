f2d8d6f6f1f52fbceb5c60619bc881b2486d28c3
xxm <2444010520@qq.com>
2022-06-15 20:52:33 +0800

    master提交

--- src/main/java/com/atguigu/GItIdeaTest.java
+++ src/main/java/com/atguigu/GItIdeaTest.java
@@ -7,5 +7,7 @@ public class GItIdeaTest {
         System.out.println(333);
         System.out.println("master");
         System.out.println("hotfix");
+        System.out.println("master,master");
+
     }
 }
